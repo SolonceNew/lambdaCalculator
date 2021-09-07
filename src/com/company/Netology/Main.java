f8607960b3ba45b0515a9c501817965d4ddf0328
Anna Fabrichnaya <fabrichnaya.anna@bk.ru>
2021-09-07 19:44:12 +0300

    added files

---
+++ src/com/company/Netology/Main.java
@@ -0,0 +1,22 @@
+package com.company.Netology;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        // выскакивает ArithmeticException  - деление на 0,
+        // поскольку результатом вычисления переменной b является 0.
+        // Для решения проблемы мы можем обработать исключение ArithmeticException (реализован ниже)
+        // 2-й способ смотрите в комментарии класса Calculator
+
+      try {
+           int c = calc.devide.apply(a, b);
+           calc.println.accept(c);
+      } catch (ArithmeticException e) {
+           System.out.println("Внимание!!! Деление на 0! На ноль делить нельзя");
+      }
+
+    }
+}
