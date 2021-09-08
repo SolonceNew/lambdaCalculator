679e202b90ed247982be6626f3a0c8202a713bf9
Anna Fabrichnaya <fabrichnaya.anna@bk.ru>
2021-09-08 11:32:10 +0300

    correct a typo abs

--- src/com/company/Netology/Calculator.java
+++ src/com/company/Netology/Calculator.java
@@ -9,20 +9,9 @@ public class Calculator {
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
     BinaryOperator<Integer> devide = (x, y) -> x / y;
-// 2-й способ:
-//  BinaryOperator<Integer> devide = (x, y) -> {
-//      int result;
-//      if(y == 0) {
-//         System.out.println("Внимание! Деление на 0! Делить на 0 нельзя!");
-//         result = 0;
-//     } else {
-//     result =  x / y;
-//     }
-//     return result;
-// };
 
     UnaryOperator<Integer> pow = x -> x * x;
-    UnaryOperator<Integer> abs = x -> x > 0 ? x : 0 * -1;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
 
     Predicate<Integer> isPositive = x -> x > 0;
